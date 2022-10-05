f428f981295c34d5907ce5aefb6673b5b2b0bffc
Marina Ruiz <marruiart@hotmail.com>
2022-10-05 12:03:01 +0200

    Ejercicio1 added

---
+++ Ejercicio1.java
@@ -0,0 +1,10 @@
+/**
+ * Ejercicio 1
+ * Escribe una programa que muestre tu nombre por pantalla.
+ */
+
+public class Ejercicio1 {
+    public static void main(String args[]) {
+        System.out.println("Hello world!");
+    }
+}
