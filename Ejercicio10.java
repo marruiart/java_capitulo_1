c431b2b9ae3a5eba82ce2f7615444f35bb04c0d7
Marina Ruiz <marruiart@hotmail.com>
2022-10-06 10:04:52 +0200

    Ejercicio 9 and 10 added + gitignore

---
+++ Ejercicio10.java
@@ -0,0 +1,40 @@
+/**
+* This program prints a coloured picture of a toad on the standard output
+ */
+
+public class Ejercicio10 {
+    public static void main(String args[]) {
+        System.out.println("                  \033[30m@@@@@@@@@@@@@@@@@@@@@@@@                  ");
+        System.out.println("          \033[30m@@@@@@@@\033[37mWWWWWWWW\033[31mOOOOOOOO\033[37mWWWWWWWW\033[30m@@@@@@@@          ");
+        System.out.println("      \033[30m@@@@\033[31mOOOO\033[37mWWWWWWWWWWWW\033[91mBBBBBBBB\033[37mWWWWWWWWWWWW\033[31mOOOO\033[30m@@@@      ");
+        System.out.println("    \033[30m@@\033[31mOOOO\033[91mBBBB\033[37mWWWWWWWW\033[91mBBBBBBBBBBBBBBBB\033[37mWWWWWWWW\033[91mBBBB\033[31mOOOO\033[30m@@    ");
+        System.out.println("  \033[30m@@@@\033[37mWWWW\033[91mBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB\033[37mWWWW\033[30m@@@@  ");
+        System.out.println("\033[30m@@\033[37mWWWWWWWWWWWW\033[91mBBBBBBBB\033[37mWWWWWWWWWWWWWWWW\033[91mBBBBBBBB\033[37mWWWWWWWWWWWW\033[30m@@");
+        System.out.println("\033[30m@@\033[37mWWWWWWWWWWWW\033[91mBBBB\033[37mWWWWWWWWWWWWWWWWWWWWWWWW\033[91mBBBB\033[37mWWWWWWWWWWWW\033[30m@@");
+        System.out.println("\033[30m@@\033[37mWWWWWWWWWWWW\033[91mBBBB\033[37mWWWWWWWWWWWWWWWWWWWWWWWW\033[91mBBBB\033[37mWWWWWWWWWWWW\033[30m@@");
+        System.out.println("\033[30m@@\033[37mWWWWWWWWWWWW\033[91mBBBB\033[37mWWWWWWWWWWWWWWWWWWWWWWWW\033[91mBBBB\033[37mWWWWWWWWWWWW\033[30m@@");
+        System.out.println("\033[30m@@\033[37mWWWWWWWWWWWW\033[91mBBBB\033[37mWWWWWWWWWWWWWWWWWWWWWWWW\033[91mBBBB\033[37mWWWWWWWWWWWW\033[30m@@");
+        System.out.println("\033[30m@@\033[37mWWWWWWWWWWWW\033[91mBBBB\033[37mWWWWWWWWWWWWWWWWWWWWWWWW\033[91mBBBB\033[37mWWWWWWWWWWWW\033[30m@@");
+        System.out.println("\033[30m@@\033[37mWWWWWWWW\033[91mBBBBBBBB\033[37mWWWWWWWWWWWWWWWWWWWWWWWW\033[91mBBBBBBBB\033[37mWWWWWWWW\033[30m@@");
+        System.out.println("\033[30m@@\033[31mOOOO\033[91mBBBBBBBBBBBBBBBB\033[37mWWWWWWWWWWWWWWWW\033[91mBBBBBBBBBBBBBBBB\033[31mOOOO\033[30m@@");
+        System.out.println("\033[30m@@\033[31mOOOOOOOO\033[30m@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\033[31mOOOOOOOO\033[30m@@");
+        System.out.println(" \033[30m@@@@@@@@@@@@@\033[37mWWWWWWWW\033[30m@@@@\033[37mWWWWWWWW\033[30m@@@@\033[37mWWWWWWWW\033[30m@@@@@@@@@@@@@ ");
+        System.out.println("          \033[30m@@\033[37mWWWWWWWWWW\033[30m@@@@\033[37mWWWWWWWW\033[30m@@@@\033[37mWWWWWWWWWW\033[30m@@          ");
+        System.out.println("          \033[30m@@\033[37mWWWWWWWWWW\033[30m@@@@\033[37mWWWWWWWW\033[30m@@@@\033[37mWWWWWWWWWW\033[30m@@          ");
+        System.out.println("          \033[30m@@\033[37mWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW\033[30m@@          ");
+        System.out.println("          \033[30m@@\033[37mWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW\033[30m@@          ");
+        System.out.println("           \033[30m@@@\033[37mWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW\033[30m@@@           ");
+        System.out.println("            \033[30m@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@            ");
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
